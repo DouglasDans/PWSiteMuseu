b658d3a96d5522dbc73102faa901e4275bfe56f3
DouglasDans <douglas.dans@gmail.com>
2020-08-19 00:08:00 -0300

    logoff e roteirista maluco

--- area-restrita/index-area-restrita.php
+++ area-restrita/index-area-restrita.php
@@ -1,18 +1,227 @@
-<!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <?php
-            include("valida-sentinela.php");
-        ?>
-        <h1>Index da Área Restrita, boas compras!</h1>
-        <div class="embed-responsive embed-responsive-16by9">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/B3gAZvncfa0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Home - Salvador Dalí</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
+</head>
+
+<body class="bg-pers1 text-white">
+    <header class="justify-content-end">
+        <div class="nav-container justify-content-end">
+            <nav class="navbar navbar-expand-md navbar-white p-0 justify-content-end bg-blur bg-pers0 p-2">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4">
+                            <a class="nav-link" href="index.html"><img src="../img/dalidaonde.png" height="40px"></a>
+                        </div>
+                        <div class="col">
+                            <a class="nav-link mt-2" href="index.html">Home</a>
+                        </div>
+                        <div class="col">
+                            <a class="nav-link mt-2" href="produtos.html">Produtos</a>
+                        </div>
+                        <div class="col">
+                            <a class="nav-link mt-2" href="quemsomos.html">Quem Somos</a>
+                        </div>
+                        <div class="col">
+                            <a class="nav-link mt-2" href="contato.html">Fale Conosco</a>
+                        </div>
+                        <div class="col p-1 bg-dark rounded-lg">
+                            <img src="../img/loadsc7.png" class=" rounded-circle mr-3" style="height: 30px;"
+                                title="Seu Perfil">
+                            <font class=" text-white">Olá Vanessa </font><small><a href="../logout.php" style="top:30px"
+                                    class=" position-absolute nav-link text-decoration-none text-white mt-2 ml-4">Sair</a>
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
-        <a href="../logout.php">Sair</a>
-    </body>
+    </header>
+
+    <main>
+        <div id="home-img">
+            <div class="carousel-caption">
+                <blockquote class="blockquote text-right">
+                    <h1 class="display-1">Tá maluco professor?</h1>
+                    <footer class="blockquote-footer" style="color: white;">Thiago</cite></footer>
+                </blockquote>
+            </div>
+        </div>
+        <div class="container-fluid divDestaqueMenu pb-4 pt-4">
+
+            <div class="row text-center pb-5">
+                <h2>No nosso museu você vai encontrar</h2>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <div class="gta-gCard text-center p-1 rounded-sm">
+                        <a href="pagina0.3.html" class=""><img src="../img/salvador-dali-redondo.jpg" width="140px"
+                                height="140px" class="imgDestaqueMenu p-0 mx-auto" alt="..."></a>
+                        <div>
+                            <div class="text-center">
+                                <p class="h2">Biografia</p>
+                                <p>Aprenda quem foi e como viveu Salvador Dalí</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="gta-gCard text-center p-1 rounded-sm">
+                        <a href="pagina0.3.html" class=""><img src="../img/fachada_museu_dali.jpg" width="140px"
+                                height="140px" class="imgDestaqueMenu p-0 mx-auto" alt="..."></a>
+                        <div>
+                            <div class="text-center">
+                                <p class="h2">Ingressos</p>
+                                <p>Adquira o seu passe para o museu digital de Dalí</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="gta-gCard text-center p-1 rounded-sm">
+                        <a href="pagina0.3.html" class=""><img src="../img/obras/A persistência da memória.jpg"
+                                width="140px" height="140px" class="imgDestaqueMenu p-0 mx-auto" alt="..."></a>
+                        <div>
+                            <div class="text-center">
+                                <p class="h2">Acervo</p>
+                                <p>Visite as obras do grande artista do surrealismo</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="text-left gta-gCard text-center p-1 rounded-sm">
+                        <a href="https://etecspgov-my.sharepoint.com/:f:/g/personal/douglas_sousa43_etec_sp_gov_br/En-ALtGgqtdNrzdDAUyLWQkBFQa9JqsBIAX51tiKqRILKA?e=UdF0qS"
+                            class=""><img src="../img/fc4a11c8-b099-4ab8-982d-cbee960f389f.jpg" width="140px"
+                                height="190px" class="rounded-lg p-0 mx-auto" alt="..."></a>
+                        <div class="ml-2">
+                            <div class="font-weight-bold text-center">
+                                GTA Etec Códiogo-Fonte<br>
+                                <small style="font-size: 60%">Todas as imagens e vídeos usados o jogo</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="text-left gta-gCard text-center p-1 rounded-sm">
+                        <a href="https://etecspgov-my.sharepoint.com/:u:/g/personal/douglas_sousa43_etec_sp_gov_br/EUHNu6UXYGNGlYAGg1Q-m3QB0scvynpkMpeu5txlclC8QA"
+                            class=""><img src="../img/onlie.png" width="140px" height="190px"
+                                class="rounded-lg p-0 mx-auto" alt="..."></a>
+                        <div class="ml-2">
+                            <div class="font-weight-bold text-center">
+                                GTA Etec 1.1 Online<br>
+                                <small style="font-size: 60%">Lançamento: Abril de 2020</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- DAQUI PRA BAIXO TA TUDO BUGADO, DPS EU TENTO ARRUMAR, E PUTA RODAPÉ FEIO EM DOUGLAS, ESPERAVA MAIS DE VOCÊ -->
+
+        <div class="parceria container-fluid flush-with-above p-5 bg-pers0">
+            <div class="row">
+                <div class="col-3">
+                    <img src="../img/DD-Paralax.png" class="img-fluid">
+                </div>
+                <div class="col-6 text-center">
+                    <h1 class="display-5">UMA PARCERIA INÉDITA</h1>
+                    <small>O roteirista de 2020 tá maluco</small>
+                </div>
+                <div class="col-3">
+                    <img src="../img/logoMista.png" class="img-fluid">
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid flush-with-above p-5 text-right">
+            <div class="row justify-content-around align-items-center">
+                <div class="col-12 col-md-5 order-md-2 mb-5 mb-md-0">
+                    <span class="title-decorative">GTA Etec 0.5</span>
+                    <h5 class="h1">GTA Etec: Visítas Técnicas com o CJ</h5>
+                    <p class="lead text-justify">
+                        Essa foi a primeira tentativa de criar uma expansão pro GTA Etec, não se sabe ao certo, mas essa
+                        versão
+                        foi lançada durante a 0.4/0.5 do jogo normal (mais ou menos em setembro/outubro de 2019). Ela
+                        permitia que
+                        o jogador viajasse com o CJ para todos os mapas do GTA de PS2, além dos mapas de Bully e
+                        Manhunt, batendo
+                        o recorde de 13Gb de jogo.
+                    </p>
+                </div>
+                <div class="col-12 col-md-5 order-md-1">
+                    <img src="https://preview.ibb.co/dOW2sQ/GTAUG.png" alt="" height="300px">
+                    <br><small>*imagem ilustrativa, esta versão foi totalmente perdida</small>
+                </div>
+            </div>
+        </div>
+
+        <div class="container flush-with-above p-5 bg-pers">
+            <div class="row justify-content-around align-items-center">
+                <div class="col-12 col-md-6 col-lg-5 text-center text-md-left section-intro">
+                    <span class="title-decorative">GTA Etec 1.0</span>
+                    <h3 class="h1">GTA Etec Education</h3>
+                    <span class="lead text-justify">
+                        Isso ninguém imaginava, nessa época, o jogo normal estava crescendo cada dia mais a quantidade
+                        de GB, com
+                        isso foi prometido uma versão lite, só que foi cancelada e tranformada no GTA Etec Education,
+                        que sim,
+                        isso que você está pensando, uso educacional, infelizmente não deu certo por causa da
+                        dificuldade de criar
+                        missões rapidamente.
+                    </span>
+                </div>
+                <div class="col-12 col-md-5 order-md-1">
+                    <img src="https://vignette.wikia.nocookie.net/dyomworld/images/6/61/PgPtP.png/revision/latest/scale-to-width-down/340?cb=20110827122357"
+                        alt="" height="300px">
+                    <br><small>*imagem ilustrativa, perdi tbm foi mals</small>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid text-center pb-2" style="background: white;">
+            <img src="../img/Americanas-empresas_home_marcas_01_samsung.png">
+            <img src="../img/Americanas-empresas_home_marcas_02_dell.png">
+            <img src="../img/Americanas-empresas_home_marcas_03_acer.png">
+            <img src="../img/Americanas-empresas_home_marcas_04_apple.png">
+            <img src="../img/Americanas-empresas_home_marcas_05_motorola.png">
+            <img src="../img/Americanas-empresas_home_marcas_06_LG.png">
+            <img src="../img/Americanas-empresas_home_marcas_07_brastemp.png">
+            <img src="../img/Americanas-empresas_home_marcas_11_lenovo.png">
+            <img src="../img/Americanas-empresas_MARCAS_marcas_13_microsoft.png">
+            <img src="../img/Americanas-empresas_MARCAS_marcas_14_intel.png">
+            <img src="../img/Americanas-empresas_MARCAS_marcas_14_multilaser.png">
+        </div>
+
+
+
+    </main>
+</body>
+<footer class="text-white bg-dark">
+    <div class="container-fluid row text-left pt-4">
+        <div class="col-6 float-left">
+            <img src="../img/DD-Rmk-W.png" height="30px">
+        </div>
+        <div class="col-6 text-right">
+            <img src="../img/logoMista.png" height="30px" class="ml-5">
+        </div>
+    </div>
+    <div class="container-fluid row text-center p-3">
+        <small>DD Livros é uma marca registrada Paralax Company © 2019</small>
+        <br>
+        <small>© 2019 Paralax Design Site 5 é uma marca Grupo Liga</small>
+    </div>
+    <img src="img/fBorder.png" width=100% height="2px">
+</footer>
+
 </html>
