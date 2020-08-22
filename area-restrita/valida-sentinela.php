9d59f3cd4512b472acc565d6555d3b002d9dce65
DouglasDans <douglas.dans@gmail.com>
2020-08-22 15:20:27 -0300

    rei leão 3

--- area-restrita/valida-sentinela.php
+++ area-restrita/valida-sentinela.php
@@ -2,7 +2,7 @@
 
     session_start();
     if (($_SESSION['login-session'] != 'adm') || 
-        ($_SESSION['senha-session'] != '123')){
+        ($_SESSION['senha-session'] != '1234')){
             header("Location: ../index.php");
         }
 ?>
